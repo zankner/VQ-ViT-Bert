0ebf4e325b10bda50fea958dd18c10bebdae504e
Zack Ankner <zackankner@gmail.com>
2021-03-10 13:30:42 -0500

    encoder

--- vae/vae.py
+++ vae/vae.py
@@ -5,6 +5,8 @@ import torch.nn.functional as F
 from einops import rearrange, repeat
 from einops.layers.torch import Rearrange
 
+from collections import OrderedDict
+
 
 class ResBlock(nn.Module):
     def __init__(self, in_features, out_features):
@@ -39,3 +41,65 @@ class ResBlock(nn.Module):
         out = self.relu(out)
 
         return out
+
+
+class Encoder(nn.Module):
+    def __init__(self, vocab_size, num_blocks=2, feature_dim=64, channels=3):
+        super(Encoder, self).__init__()
+
+        # Input conv
+        self.input_conv = nn.Conv2d(channels, feature_dim, kernel_size=7)
+
+        # Blocks
+        self.blocks = nn.Sequential(
+            OrderedDict([
+                ('group_1',
+                 nn.Sequential(
+                     OrderedDict([
+                         *[(f'block_{i+1}',
+                            ResBlock(1 * feature_dim, 1 * feature_dim))
+                           for i in range(num_blocks)]
+                     ]))),
+                ('group_2',
+                 nn.Sequential(
+                     OrderedDict([
+                         *[(f'block_{i+1}',
+                            ResBlock(
+                                1 * feature_dim if i == 0 else 2 * feature_dim,
+                                2 * feature_dim)) for i in range(num_blocks)]
+                     ]))),
+                ('group_3',
+                 nn.Sequential(
+                     OrderedDict([
+                         *[(f'block_{i+1}',
+                            ResBlock(
+                                2 * feature_dim if i == 0 else 4 * feature_dim,
+                                4 * feature_dim)) for i in range(num_blocks)]
+                     ]))),
+                ('group_4',
+                 nn.Sequential(
+                     OrderedDict([
+                         *[(f'block_{i+1}',
+                            ResBlock(
+                                4 * feature_dim if i == 0 else 8 * feature_dim,
+                                8 * feature_dim)) for i in range(num_blocks)]
+                     ])))
+            ]))
+
+        # Output conv
+        self.output_conv = nn.Conv2d(8 * feature_dim,
+                                     vocab_size,
+                                     kernel_size=1)
+
+        # Activations
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.input_conv(x)
+        x = self.relu(x)
+
+        x = self.blocks(x)
+
+        x = self.output_conv(x)
+
+        return x
