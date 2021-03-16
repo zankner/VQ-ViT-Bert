69e7669a4fc58062abdceb338d3019682c983119
Zack Ankner <zackankner@gmail.com>
2021-03-16 17:55:17 -0400

    tokens data loader

---
+++ vit/TokensDataset.py
@@ -0,0 +1,28 @@
+import torch
+import os
+
+
+class TokensDataset(torch.utils.data.Dataset):
+    """VQ-VAE tokens dataset"""
+    def __init__(self, root_dir, extension="pt"):
+        if not os.path.isdir(root_dir):
+            raise ValueError("Root dir specified does not exist")
+
+        self.root_dir = root_dir
+        self.extension = extension
+        self.samples = self._get_files()
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+        tokens_path = os.path.join(self.root_dir, self.samples[idx])
+        tokens = torch.load(tokens_path)
+        return tokens
+
+    def _get_files(self):
+        files = os.listdir(self.root_dir)
+        files = [
+            file for file in files if file.lower().endswith(self.extension)
+        ]
+        return files
--- vit/__init__.py
+++ vit/__init__.py
@@ -1,2 +1,3 @@
 from vit.vit import ViT
 from vit.mpp import MPP
+from vit.TokensDataset import TokensDataset
