455eafdb8c0bfcf2786419decd915a09aa0ee007
Zack Ankner <zackankner@gmail.com>
2021-03-23 11:49:16 -0400

    train val split of datasets without official split

--- vae/train.py
+++ vae/train.py
@@ -3,8 +3,9 @@ import torch
 import torch.nn as nn
 from torch import optim
 from torch.optim.lr_scheduler import StepLR
-from torchvision import transforms, datasets
 from torch.utils.tensorboard import SummaryWriter
+from torch.utils.data import random_split
+from torchvision import transforms, datasets
 import datetime
 from vae import VQVae
 from vae.model_utils import train_step, validate_step
@@ -26,19 +27,23 @@ def train(args):
         [transforms.CenterCrop(32),
          transforms.ToTensor(), normalize])
 
-    train_dataset = datasets.CIFAR10(args.data_dir,
-                                     train=True,
-                                     transform=train_transforms,
-                                     download=True)
+    dataset = datasets.CIFAR10(args.data_dir,
+                               train=True,
+                               transform=train_transforms,
+                               download=True)
+    train_len = int(0.8 * len(dataset))
+    train_dataset, val_dataset = random_split(
+        dataset, [train_len, len(dataset) - train_len])
+
     train_loader = torch.utils.data.DataLoader(train_dataset,
                                                batch_size=args.batch_size)
 
-    test_dataset = datasets.CIFAR10(args.data_dir,
-                                    train=False,
-                                    transform=test_transforms,
-                                    download=True)
-    test_loader = torch.utils.data.DataLoader(test_dataset,
-                                              batch_size=args.batch_size)
+    val_dataset = datasets.CIFAR10(args.data_dir,
+                                   train=False,
+                                   transform=test_transforms,
+                                   download=True)
+    val_loader = torch.utils.data.DataLoader(val_dataset,
+                                             batch_size=args.batch_size)
 
     log_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
     summary_path = os.path.join(args.summary_dir, log_time)
@@ -66,13 +71,13 @@ def train(args):
                    writer, args)
         scheduler.step()
 
-        val_loss = validate_step(test_loader, model, criterion, device, args)
+        val_loss = validate_step(val_loader, model, criterion, device, args)
 
         # Checkpoint model
         torch.save(
             {
                 'epoch': epoch,
-                'model_state_dict': mpp.state_dict(),
+                'model_state_dict': model.state_dict(),
                 'optimizer_state_dict': optimizer.state_dict(),
                 'loss': val_loss,
             }, os.path.join(checkpoint_dir, "checkpoint.pt"))
